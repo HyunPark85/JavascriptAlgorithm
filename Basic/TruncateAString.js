b57d895c168d43d2181dd82f18e9ecb404376896
HyunPark85 <parkhyun148@gmail.com>
2019-01-25 18:29:13 -0500

    commit

--- Basic/TruncateAString.js
+++ Basic/TruncateAString.js
@@ -13,3 +13,11 @@ function truncateString(str, num) {
   }
   
   truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+  function truncateString(str, num) {
+    if(str.length <= num)
+      return str;
+    return str.substr(0,num) + "...";
+  }
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
